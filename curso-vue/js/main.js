b9b4738683d3f8b299b9305191d9a965c47b6a59
kryuchcov <kryuchcov@gmail.com>
2022-08-26 13:39:51 -0500

    Seguimiento del proyecto

--- curso-vue/js/main.js
+++ curso-vue/js/main.js
@@ -12,10 +12,27 @@ const app = Vue.createApp({
       favorites: new Map(),
     };
   },
-//   Propiedades computadas
-  comput: {
+  // Fase del ciclo de vida: created()
+  created() {
+    const savedFavorites = JSON.parse((window.localStorage.getItem("favorites")));
+    if (savedFavorites?.length) {
+      const favorites = new Map(savedFavorites.map(favorite => [favorite.id, favorite]));
+      this.favorites = favorites
+    }
+  },
+  // Propiedades computadas
+  computed: {
+    // isFavorite() {
+    //   if (this.result != null)
+    //     return this.favorites.has(this.result.id)
+    //   else
+    //     return true
+    // },
     isFavorite() {
         return this.favorites.has(this.result.id)
+    }, 
+    allFavorites() {
+      return Array.from(this.favorites.values())
     }
   },
   methods: {
@@ -26,7 +43,7 @@ const app = Vue.createApp({
         const response = await fetch(API + this.search);
         if (!response.ok) throw new Error("User not found");
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         this.result = data; //Trae del API la data correspondiente
       } catch (error) {
         this.error = error;
@@ -37,7 +54,24 @@ const app = Vue.createApp({
 
     //Función para añadir a favoritos a varios usuarios de github
     addFavorite() {
-      this.favorites.set(this.result.id, this.result); 
+      this.favorites.set(this.result.id, this.result);
+      this.updateStorage();
+    },
+
+    // Función para eliminar favoritos
+    removeFavorite() {
+      this.favorites.delete(this.result.id);
+      this.updateStorage();
+    },
+
+    // Función para cargar la información del usuario favorito dentro de "component"
+    showFavorite(favorite) {
+      this.result = favorite
+    },
+
+    // Función para almacenar los datos de navegación
+    updateStorage() {
+      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
     }
   },
 });
