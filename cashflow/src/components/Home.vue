72a5a374086725467285b8f31d0f7b3ecececc96
Erika Miranda <nohemileonla@gmai.com>
2022-09-11 15:38:34 -0500

    cashflow: app móvil

---
+++ cashflow/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+    </Layout>
+</template>
+
+<script>
+    import Layout from "./Layout.vue"
+    import Header from "./Header.vue"
+    
+    export default {
+        components: {
+        Layout,
+        Header,
+    }
+    }
+
+</script>
---
+++ cashflow/src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="splashscreen">
+        <img src="@/assets/cashflow-icon_070076db-e83a-4afa-b18d-ee058c2b646e.svg" alt="Logo">
+        <h1><span>Cash</span>Flow</h1>
+    </div>
+</template>
+
+<style>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
