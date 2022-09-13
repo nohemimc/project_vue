3691e7f2f2023e415bdf96bfa3006035f6e71ba3
Erika Miranda <nohemileonla@gmai.com>
2022-09-12 23:28:35 -0500

    puntos terminados

---
+++ card/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+
+    <!-- Uso del emit, nombre del evento personalizado y la función a realizar en el componente hijo "HelloWorld" -->
+    <HelloWorld @notification="message"/>
+
+    <!-- Ejemplo del watcher -->
+    <div>
+      <input type="text" @keypress="valueInput">
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      valueInput: "",
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+
+    // Función del evento personalizado: "notification"
+    message() {
+      console.log("Limpieza del DOM FINAL");
+    }
+  },
+  watch:{
+    entrada: function () {
+      console.log(this.valueInput);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ card/src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+
+    <!-- Cambio del color del titulo -->
+    <div v-for="(btn, index) in botones" :key="index" class="select-age">
+      <h1  :class="[btn.className, { selected: selectedIndex === index }]" >Cambia el titulo</h1>
+      <div class="div_buttons" >
+        <button
+          class="botones"
+          @click="selectedIndex = index"
+        >
+          {{ btn.btnName }}
+        </button>
+      </div>
+    </div>
+
+    <!-- Uso de los emits -->
+    <div>
+      <p>Uso de emit</p>
+      <button @click="notify">{{example}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedIndex: -1,
+      botones: [
+        {
+          btnName: "Blue",
+          className: "btn-negro",
+          state: 0,
+        },
+        {
+          btnName: "Red",
+          className: "btn-azul",
+          state: 1,
+        },
+        {
+          btnName: "Green",
+          className: "btn-verde",
+          state: 2,
+        },
+      ],
+
+      example: "Limpiar DOM"
+    };
+  },
+  methods: {
+    notify() {
+      this.$emit("notification", this.example)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.div_buttons{
+  display: inline-block;
+}
+.botones {
+  background-color: gray;
+  color: white;
+  width: 5rem;
+  height: 3rem;
+  justify-content: space-between;
+}
+.selected.btn-negro {
+  background: white;
+  color: black;
+}
+
+.selected.btn-azul {
+  background: white;
+  color: red;
+}
+
+.selected.btn-verde {
+  background: white;
+  color: green;
+}
+</style>
